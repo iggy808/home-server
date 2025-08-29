22469d26ffbfc467619bd3a537441939c1fac58e
iggy808 <63073663+iggy808@users.noreply.github.com>
2025-08-28 23:05:35 -0500

    Add main ledger table, edit action UI, pagination UI

--- src/routes/modules/finances/+page.svelte
+++ src/routes/modules/finances/+page.svelte
@@ -10,7 +10,7 @@
 
 <div id="module-container" class="mx-10 my-4 grid grid-cols-2 grid-rows-3 gap-4">
 
-    <div id="finances-chart-container" class="border min-h-[25vh] col-span-2 bg-section">
+    <div id="finances-chart-container" class="border min-h-[29vh] col-span-2 bg-section">
         <canvas id="finances-chart" use:chartRender={ data.financeChart } ></canvas>
     </div>
 
@@ -24,7 +24,7 @@
             </div>
         </div>
         <hr class="w-[95%] mx-auto"/>
-        <div id="expenses-snapshot-list-container" class="mx-6 my-2 px-2 py-2">
+        <div id="expenses-snapshot-table-container" class="mx-6 my-2 px-2 py-2">
             <table class="table-fixed w-full h-full border">
                 <thead class="bg-table-header">
                     <tr>
@@ -54,6 +54,11 @@
                         <td class="text-left border p-1">25-08-2025</td>
                         <td class="text-left border p-1">$15</td>
                     </tr>
+                    <tr>
+                        <td class="text-left border p-1">Wing Stop</td>
+                        <td class="text-left border p-1">24-08-2025</td>
+                        <td class="text-left border p-1">$10,000</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -69,7 +74,7 @@
             </div>
         </div>
         <hr class="w-[95%] mx-auto"/>
-        <div id="savings-snapshot-list-container" class="mx-6 my-2 px-2 py-2">
+        <div id="savings-snapshot-table-container" class="mx-6 my-2 px-2 py-2">
             <table class="table-fixed w-full h-full border">
                 <thead class="bg-table-header">
                     <tr>
@@ -99,12 +104,143 @@
                         <td class="text-left border p-1">25-08-2025</td>
                         <td class="text-left border p-1">$100</td>
                     </tr>
+                    <tr>
+                        <td class="text-left border p-1">Scheduled Deposit</td>
+                        <td class="text-left border p-1">25-08-2025</td>
+                        <td class="text-left border p-1">$100</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
     </div>
     
-    <div id="finances-table-container" class="border col-span-2 bg-section">
-        <p>finances table</p>
+    <div id="finances-ledger-container" class="border col-span-2 bg-section">
+        <div id="finances-ledger-title-container">
+            <h1 class="px-8 py-3 text-xl">Ledger</h1>
+        </div>
+        <hr class="w-[95%] mx-auto"/>
+        <div id="finances-ledger-table-container" class="mx-12 mt-4">
+            <table class="table-fixed w-full h-full border">
+                <thead class="bg-table-header">
+                    <tr>
+                        <th class="text-left border p-1">Description</th>
+                        <th class="text-left border p-1">Date</th>
+                        <th class="text-left border p-1">Amount</th>
+                        <th class="text-left border p-1">Actions</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-table-body">
+                    <tr>
+                        <td class="text-left border p-1">Scheduled Deposit</td>
+                        <td class="text-left border p-1">28-08-2025</td>
+                        <td class="text-left border p-1">$100</td>
+                        <td class="border p-1">
+                            <div class="grid grid-cols-2 grid-rows-1">
+                                <button type="button" class="bg-interactive mx-8 rounded-xl">
+                                    <span class="text-m">Edit</span>
+                                </button>
+                                <button type="button" class="bg-interactive mx-8 rounded-xl">
+                                    <span class="text-m">Delete</span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-left border p-1">Scheduled Deposit</td>
+                        <td class="text-left border p-1">27-08-2025</td>
+                        <td class="text-left border p-1">$100</td>
+                        <td class="border p-1">
+                            <div class="grid grid-cols-2 grid-rows-1">
+                                <button type="button" class="bg-interactive mx-8 rounded-xl">
+                                    <span class="text-m">Edit</span>
+                                </button>
+                                <button type="button" class="bg-interactive mx-8 rounded-xl">
+                                    <span class="text-m">Delete</span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-left border p-1">Scheduled Deposit</td>
+                        <td class="text-left border p-1">26-08-2025</td>
+                        <td class="text-left border p-1">$100</td>
+                        <td class="border p-1">
+                            <div class="grid grid-cols-2 grid-rows-1">
+                                <button type="button" class="bg-interactive mx-8 rounded-xl">
+                                    <span class="text-m">Edit</span>
+                                </button>
+                                <button type="button" class="bg-interactive mx-8 rounded-xl">
+                                    <span class="text-m">Delete</span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-left border p-1">Scheduled Deposit</td>
+                        <td class="text-left border p-1">25-08-2025</td>
+                        <td class="text-left border p-1">$100</td>
+                        <td class="border p-1">
+                            <div class="grid grid-cols-2 grid-rows-1">
+                                <button type="button" class="bg-interactive mx-8 rounded-xl">
+                                    <span class="text-m">Edit</span>
+                                </button>
+                                <button type="button" class="bg-interactive mx-8 rounded-xl">
+                                    <span class="text-m">Delete</span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="text-left border p-1">Scheduled Deposit</td>
+                        <td class="text-left border p-1">25-08-2025</td>
+                        <td class="text-left border p-1">$100</td>
+                        <td class="border p-1">
+                            <div class="grid grid-cols-2 grid-rows-1">
+                                <button type="button" class="bg-interactive mx-8 rounded-xl">
+                                    <span class="text-m">Edit</span>
+                                </button>
+                                <button type="button" class="bg-interactive mx-8 rounded-xl">
+                                    <span class="text-m">Delete</span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div id="finances-ledger-table-footer" class="mx-12 mb-1">
+            <div id="finances-ledger-table-pagination-container" class="w-1/4 mt-2">
+                <div class="grid grid-cols-6 grid-rows-1">
+                    <div>
+                        <p>Page:</p>
+                    </div>
+                    <div class="justify-self-center">
+                        <button type="button" class="bg-interactive w-6 h-6 rounded-full">
+                            <span class="text-m">1</span>
+                        </button>
+                    </div>
+                    <div class="justify-self-center">
+                        <button type="button" class="bg-interactive w-6 h-6 rounded-full">
+                            <span class="text-m">2</span>
+                        </button>
+                    </div>
+                    <div class="justify-self-center">
+                        <button type="button" class="bg-interactive w-6 h-6 rounded-full">
+                            <span class="text-m">3</span>
+                        </button>
+                    </div>
+                    <div class="justify-self-center">
+                        <button type="button" class="bg-interactive w-6 h-6 rounded-full">
+                            <span class="text-m">...</span>
+                        </button>
+                    </div>
+                    <div class="justify-self-center">
+                        <button type="button" class="bg-interactive w-6 h-6 rounded-full">
+                            <span class="text-m">9</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
